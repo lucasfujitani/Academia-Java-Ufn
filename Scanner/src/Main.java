39041b8734a9f4838193a97886e72700e193d388
Lucas Fujitani <castrofujitani@gmail.com>
2023-10-28 14:16:58 -0300

    Calculadora Update

---
+++ Scanner/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("em que ano estamos?");
+        String ano = scanner.nextLine();
+        System.out.println("em que ano nasceu?");
+        String anoN = scanner.nextLine();
+
+       s.soma()
+
+        System.out.println("o ano é " +s.soma());
+    }
+}
