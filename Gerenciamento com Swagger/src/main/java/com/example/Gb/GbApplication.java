a8f087325dcd358102bbfbb17fc1a5bc804b57c9
Lucas Fujitani <castrofujitani@gmail.com>
2023-11-29 19:52:21 -0300

    arquivos

---
+++ Gerenciamento com Swagger/src/main/java/com/example/Gb/GbApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbApplication.class, args);
+	}
+
+}
